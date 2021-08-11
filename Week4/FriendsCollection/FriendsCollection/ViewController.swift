0202438cd7ecaf856d96005e59d3727fdf41a48e
김지수 <kingwltn@naver.com>
2021-08-11 18:22:18 +0900

    Week4_CollectionView_Layout

--- Week4/FriendsCollection/FriendsCollection/ViewController.swift
+++ Week4/FriendsCollection/FriendsCollection/ViewController.swift
@@ -9,13 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
   @IBOutlet weak var collectionView: UICollectionView!
-//  var numberofCell : Int = 10
+  //  var numberofCell : Int = 10
   var friends : [Friend] = []
   let cellIdentifier : String = "cell"
   
   override func viewDidLoad() {
     super.viewDidLoad()
     self.collectionView.dataSource  = self
+    
+    let flowLayout : UICollectionViewFlowLayout
+    flowLayout = UICollectionViewFlowLayout()
+    flowLayout.sectionInset = UIEdgeInsets.zero
+    flowLayout.minimumInteritemSpacing = 10
+    flowLayout.minimumLineSpacing = 10
+    
+    let halfWidth : CGFloat = UIScreen.main.bounds.width / 2.0
+    flowLayout.estimatedItemSize = CGSize(width: halfWidth - 30, height: 90)
+    self.collectionView.collectionViewLayout = flowLayout
+    
     
     let jsonDecoder: JSONDecoder = JSONDecoder()
     guard let dataAsset: NSDataAsset = NSDataAsset(name: "friends") else {
@@ -32,12 +43,6 @@ class ViewController: UIViewController {
   
 }
 
-//extension ViewController : UICollectionViewDelegate {
-//  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//    self.numberofCell += 1
-//    collectionView.reloadData()
-//  }
-//}
 
 extension ViewController : UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
