63bfabf6ad13381ae90ce2da9aa1206a4e2507a8
김지수 <kingwltn@naver.com>
2021-08-11 18:24:47 +0900

    Assign_4 셋팅

---
+++ Week4/Assign4_Album/Assign4_Album/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assign4_Album
+//
+//  Created by 김지수 on 2021/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
